2e31e95ce0e4ddb6c2209956f2103db3fc340841
Johannes Barthelmes <johannes@jbarthelmes.de>
2020-03-02 09:21:43 +0100

    add poloniexlendingbot

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  poloniexlendingbot = pkgs.callPackage ./pkgs/poloniexlendingbot { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
